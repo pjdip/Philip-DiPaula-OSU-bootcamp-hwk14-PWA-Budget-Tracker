20813b1d7abc31d6250a06b1fd91b703966ab45a
pjdip <pjdip1@gmail.com>
2021-02-28 14:40:28 -0500

    service-worker install

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Philip-DiPaula-OSU-bootcamp-hwk14-PWA-Budget-Tracker
 
+service-worker:
+https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+
+manifest:
 https://developer.mozilla.org/en-US/docs/Web/Manifest
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,41 @@
 console.log("hello from your service worker ;)");
+
+// Defining caches and files
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+const TRANSACTIONS = "transactions";
+
+const FILES_TO_CACHE = [
+    '/',
+    'index.html',
+    'index.js',
+    'styles.css',
+    'manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// Install Event Handler
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches
+        
+            // Static, Precache Files
+            .open(PRECACHE)
+            .then(cache => {
+                cache.addAll(FILES_TO_CACHE);
+                console.log("pre-cache successful!");
+            })
+            .catch(err => console.error("pre-cache failed: ", err))
+
+            // Transaction Data
+            .open(TRANSACTIONS)
+            .then(cache => {
+                cache.add("/api/transaction");
+                console.log("transactions cache successful!");
+            })
+            .catch(err => console.error("transaction cache failed: ", err))
+            .then(self.skipWaiting())
+    );
+});
+
