c4c4a7602a682bd3ae0dc74f9f45196dd50a9410
pjdip <pjdip1@gmail.com>
2021-02-28 15:06:25 -0500

    service-worker activation

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,7 +19,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', event => {
     event.waitUntil(
         caches
-        
+
             // Static, Precache Files
             .open(PRECACHE)
             .then(cache => {
@@ -35,7 +35,45 @@ self.addEventListener('install', event => {
                 console.log("transactions cache successful!");
             })
             .catch(err => console.error("transaction cache failed: ", err))
+
+            // tells the browser to activate the service-worker
+            // immediately after installation
             .then(self.skipWaiting())
     );
 });
 
+// Activate Event Handler
+self.addEventListener('activate', event => {
+    const currentCaches = [PRECACHE, TRANSACTIONS, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+
+            // compare current cache list to list of caches
+            // remove any old caches as needed
+            .then(keyList => {
+                return Promise.all(
+                    keyList.map(key => {
+                        if (currentCaches.indexOf(key) === -1) {
+                            console.log("removing old cache data ", key);
+                            return caches.delete(key);
+                        }
+                    })
+                );
+            })
+
+/*             .then(cacheNames => {
+                return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+            })
+            .then(cachesToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete => {
+                        console.log("removing old cache data ", cacheToDelete);
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            }) */
+
+            .then(() => self.clients.claim())
+    );
+});
